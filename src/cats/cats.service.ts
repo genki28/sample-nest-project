8dc3ed320a07d6bda244f3e3d95bfdfc46295331
genki28 <taiiku39@gmail.com>
2022-05-08 03:00:23 +0900

    一旦、軽く実装してみる

--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,15 +1,18 @@
+import { PrismaService } from './../prisma.service';
 import { Injectable } from '@nestjs/common';
-import { Cat } from './interfaces/cat.interface';
+import { Cat, Prisma } from '@prisma/client';
 
 @Injectable()
 export class CatsService {
-  private readonly cats: Cat[] = [];
+  constructor(private prisma: PrismaService) {}
 
-  create(cat: Cat) {
-    this.cats.push(cat);
+  create(cat: Prisma.CatCreateInput) {
+    return this.prisma.cat.create({
+      data: cat,
+    });
   }
 
-  findAll(): Cat[] {
-    return this.cats;
+  findAll(): Promise<Cat[]> {
+    return this.prisma.cat.findMany();
   }
 }
