2db866e61233d8d352979c34c0b2d73d9e26b24e
genki28 <taiiku39@gmail.com>
2022-05-24 01:16:12 +0900

    まずは一旦設定してみる

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
 import { LoggingInterceptor } from './common/logging/logging.interceptor';
 import { logger } from './common/middleware/logger.middleware';
@@ -23,6 +24,16 @@ import { UserService } from './user/user.service';
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'scr/schema.gql'),
+      // schemaファーストの場合は以下のように記述する
+      // TODO: これって、もう少し間違いなくできるようにならないだろうか？
+      // MEMO: なお、.graphqlスクリプトの監視モードを有効にするにはいくつかの設定は必要になる
+      // typePaths: ['./**/*.graphql'],
+
+      // 冗長になるので、以下のように抽象構文からts定義を自動生成するように設定可能
+      // definitions: {
+      //   path: join(process.cwd(), 'src/graphql.ts'),
+      // },
       // 諸々の設定可能
       // debug: false,
       // playground: false,
